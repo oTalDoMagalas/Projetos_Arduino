03b9f9eed12be0ec3e0fc58d765e6f822eb54238
Gustavo Magalas Generoso <gustavomagalas.senai@gmail.com>
2024-08-05 15:12:56 -0300

    comentarios adicionados

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,11 +1,16 @@
-oid setup() {
+//Progama que envia sinal alto para saida 7,
+//aguarde 1 segundo e repita novamente
+void setup() {
+  //
   pinMode (7,OUTPUT);
 
 }
-
+//definir a parte código que se repete
 void loop() {
+ //envia sinal alto
  digitalWrite (7, HIGH);
  delay (2000);
+ //envia sinal baixo
  digitalWrite (7, LOW);
  delay (1000);
 }
